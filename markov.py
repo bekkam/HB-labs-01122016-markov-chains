5027c622daa45fed34739beee7372cd70dffa6a1
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-01-13 19:20:53 -0800

    clean up code

--- markov.py
+++ markov.py
@@ -1,23 +1,18 @@
 from random import choice
 import sys
 
+
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
 
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-    # body = ""
-    # print type(file_path[0])
+
     contents = open(file_path[0]).read()
     contents = contents.split()
-    # for item in contents:
-    #     body += item + " "
-    # print "body in open and read is ", body
-    # return body.split()
     return contents
 
-# print open_and_read_file("green-eggs.txt")
 
 def make_chains(text_string):
     """Takes input text as string; returns _dictionary_ of markov chains.
@@ -34,19 +29,14 @@ def make_chains(text_string):
 
     chains = {}
     for i in range(len(text_string)-2):
-        # print i
         bigram = (text_string[i], text_string[i + 1])
         bigram_value = text_string[i + 2]
-        # if bigram in chains.iterkeys():
         if bigram in chains.keys():
             chains[bigram].append(bigram_value)
         else:
             chains[bigram] = [bigram_value]
-    # print "chains.keys() is ", chains.keys()
     return chains
-# test_list = "this is a test this is not".split()
-# print test_list
-# print make_chains(test_list)
+
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
@@ -55,7 +45,6 @@ def make_text(chains):
     current_key = choice(chains.keys())
     text += current_key[0] + " " + current_key[1]
 
-    # while current key does not raise a key error
     while True:
         try:
             value = choice(chains[current_key])
@@ -70,11 +59,7 @@ input_path = sys.argv[1:]
 
 # # # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
-
 # # # Get a Markov chain
 chains = make_chains(input_text)
-# print chains.keys()
 # # Produce random text
 random_text = make_text(chains)
-
-print random_text
